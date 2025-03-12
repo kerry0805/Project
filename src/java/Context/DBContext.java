4ac316e1d7d681e280faea0fef4668f8afe20202
kerry0805 <160558966+kerry0805@users.noreply.github.com>
2025-03-13 03:34:53 +0700

    Đổi db ip

--- src/java/Context/DBContext.java
+++ src/java/Context/DBContext.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Context;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-/**
- *
- * @author Khuong Hung
- */
 public class DBContext {
     
-    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
- /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
     public Connection getConnection() throws Exception {
         String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
         if (instance == null || instance.trim().isEmpty()) {
@@ -24,10 +13,11 @@ public class DBContext {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         return DriverManager.getConnection(url, userID, password);
     }
-    private final String serverName = "localhost";
+
+    private final String serverName = "192.168.66.5"; // Đổi localhost thành địa chỉ IP
     private final String dbName = "SHOP2";
     private final String portNumber = "1433";
-    private final String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+    private final String instance = ""; 
     private final String userID = "sa";
     private final String password = "sa";
 }
